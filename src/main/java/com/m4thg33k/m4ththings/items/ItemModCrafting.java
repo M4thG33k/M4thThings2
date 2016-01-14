f6d703c35283ba01fe7713ddd06685f34c4e4ab2
M4thG33k <pepethemathgeek@gmail.com>
2016-01-14 16:20:14 -0600

    Added tooltip info

--- src/main/java/com/m4thg33k/m4ththings/items/ItemModCrafting.java
+++ src/main/java/com/m4thg33k/m4ththings/items/ItemModCrafting.java
@@ -1,10 +1,16 @@
 package com.m4thg33k.m4ththings.items;
 
 import com.m4thg33k.m4ththings.helpers.NameHelper;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.IIcon;
+
+import java.util.List;
 
 public class ItemModCrafting extends Item {
 
@@ -35,5 +41,12 @@ public class ItemModCrafting extends Item {
     @Override
     public String getUnlocalizedName(ItemStack stack) {
         return this.getUnlocalizedName() + "_" + stack.getItemDamage();
+    }
+
+    @SideOnly(Side.CLIENT)
+
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean par4) {
+        list.add(EnumChatFormatting.ITALIC + "Crafting item - no other use");
     }
 }
